1831f36a655ac65060f80f2cd00b0c1924d654c5
mjesiolowski <maciej.jesiolowski@gmail.com>
2019-05-17 10:37:53 +0200

    restructure code

--- main.js
+++ main.js
@@ -1,74 +1,73 @@
-// IMAGES
-const paperImg = document.querySelector(".paper");
-const rockImg = document.querySelector(".rock");
-const scissorsImg = document.querySelector(".scissors");
-const images = [...document.querySelectorAll(".images img")];
+const app = {
+   init() {
+      // IMAGES
+      this.paperImg = document.querySelector(".paper")
+      this.rockImg = document.querySelector(".rock")
+      this.scissorsImg = document.querySelector(".scissors")
+      this.images = [...document.querySelectorAll(".images img")]
 
-// SPANS
-const playerChoiceSpan = document.querySelector(".playerChoice");
-const AIChoiceSpan = document.querySelector(".computerChoice");
-let games = document.querySelector(".games")
-let wins = document.querySelector(".wins")
-let draws = document.querySelector(".draws")
-let losses = document.querySelector(".losses")
+      // SPANS
+      this.playerChoiceSpan = document.querySelector(".playerChoice")
+      this.AIChoiceSpan = document.querySelector(".computerChoice")
+      this.games = document.querySelector(".games")
+      this.wins = document.querySelector(".wins")
+      this.draws = document.querySelector(".draws")
+      this.losses = document.querySelector(".losses")
 
-const button = document.querySelector("button");
-const h1 = document.querySelector("h1");
-const h2 = document.querySelector(".yourChoice");
+      this.button = document.querySelector("button")
+      this.title = document.querySelector(".title")
+      this.h2 = document.querySelector(".yourChoice")
 
-// PLAYER CHOICE
+      this.button.addEventListener("click", () => this.startGame(this))
 
-function playerChoice() {
-   images.forEach((image => {
-      image.addEventListener("click", function (e) {
-         images.forEach(image => {
-            image.style.transform = ""
+      this.playerChoice()
+
+
+   },
+
+   playerChoice() {
+      this.images.forEach((image => {
+         image.addEventListener("click", e => {
+            this.images.forEach(image => {
+               image.style.transform = ""
+            })
+            this.playerChoiceSpan.textContent = e.target.className;
+            this.AIChoiceSpan.textContent = ""
+            image.style.transform = "scale(1.2)"
          })
-         playerChoiceSpan.textContent = e.target.className;
-         AIChoiceSpan.textContent = ""
-         image.style.transform = "scale(1.2)"
-      })
-   }))
-}
+      }))
+   },
 
-// AI CHOICE
+   AIChoice() {
+      this.AIChoiceSpan.textContent = this.images[Math.floor(Math.random() * this.images.length)].className
+   },
 
-function AIChoice() {
-   AIChoiceSpan.textContent = images[Math.floor(Math.random() * images.length)].className
+   startGame() {
+      if (!this.playerChoiceSpan.textContent)
+         return alert("What's your choice?")
+      this.AIChoice()
+      if (this.playerChoiceSpan.textContent === this.AIChoiceSpan.textContent) {
+         this.title.textContent = "DRAW!"
+         this.draws.textContent++
+      } else if (this.playerChoiceSpan.textContent === "paper" && this.AIChoiceSpan.textContent === "rock" || this.playerChoiceSpan.textContent === "rock" && this.AIChoiceSpan.textContent === "scissors" || this.playerChoiceSpan.textContent === "scissors" && this.AIChoiceSpan.textContent === "paper") {
+         this.title.textContent = "WIN! :D"
+         this.wins.textContent++
+      } else {
+         this.title.textContent = "LOOOOSER! :("
+         this.losses.textContent++
+      }
 
-}
+      this.games.textContent++
 
-// GAME RULES
-
-function game() {
-   if (!playerChoiceSpan.textContent)
-      return alert("What's your choice?")
-   AIChoice()
-   if (playerChoiceSpan.textContent === AIChoiceSpan.textContent) {
-      h1.textContent = "DRAW!"
-      draws.textContent++
-      games.textContent++
-   } else if (playerChoiceSpan.textContent === "paper" && AIChoiceSpan.textContent === "rock" || playerChoiceSpan.textContent === "rock" && AIChoiceSpan.textContent === "scissors" || playerChoiceSpan.textContent === "scissors" && AIChoiceSpan.textContent === "paper") {
-      h1.textContent = "WIN! :D"
-      wins.textContent++
-      games.textContent++
-   } else {
-      h1.textContent = "LOOOOSER! :("
-      losses.textContent++
-      games.textContent++
+      this.button.setAttribute("disabled", "true");
+      this.button.classList.add("active")
+      setTimeout(() => {
+         this.title.textContent = "What's your choice?";
+         this.AIChoiceSpan.textContent = "";
+         this.button.removeAttribute("disabled");
+         this.button.classList.remove("active")
+      }, 1500)
    }
-   button.setAttribute("disabled", "true");
-   button.classList.add("active")
-   setTimeout(function () {
-      h1.textContent = "What's your choice?";
-      AIChoiceSpan.textContent = "";
-      button.removeAttribute("disabled");
-      button.classList.remove("active")
-   }, 2000)
-
-
 }
 
-
-document.addEventListener("load", playerChoice())
-button.addEventListener("click", game)
+app.init()
