fd87b3476111c5df2f1cd1305b0fd7a71304d6d7
mjesiolowski <maciej.jesiolowski@gmail.com>
2019-07-26 15:42:35 +0200

    add local storage

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# rockPaperScissors
+# Rock-Paper-Scissors
 
-As I have been discovering some JavaScript tips lately, I decided to make this 'rock, paper, scissors' mini-game from scratch.
+## website
+https://mjesiolowski.github.io/rockPaperScissors/
 
-The project shows how to use basics of JavaScript by interacting with document object model to create a simple point-and-click game. To make the game efficient and simple I divided the code logic into two main parts: the player choice and the computer choice. The core of the logic is based on if statements, loops and setTimeout calleback.
 
+## info
+This project shows how to use basics of JavaScript to create a simple point-and-click game by interacting with document object model. To make the game efficient and simple I divided the code logic into two main parts: the player choice and the computer choice. The core of the logic is based on if statements, loops and setTimeout callback. The data is kept in local storage.
+
+## rules
 Rules:
 - make a choice by clicking on one of the figures
 - click PLAY to see the computer choice
 - you can see the statistics at the bottom of the screen
 
-
 Technology:
-HTTML5, CSS, vanilla JS, RWD
-
-
-https://mjesiolowski.github.io/rockPaperScissors/
+HTTML5, SASS + BEM, JavaScript, Gulp
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,14 +13,23 @@ const app = {
       this.wins = document.querySelector(".wins")
       this.draws = document.querySelector(".draws")
       this.losses = document.querySelector(".losses")
+      this.stats = [this.games, this.wins, this.draws, this.losses]
 
       this.button = document.querySelector("button")
       this.title = document.querySelector(".title")
 
       this.button.addEventListener("click", () => this.startGame(this))
 
+      this.getStorage()
       this.handlePlayerChoice()
+   },
 
+   getStorage() {
+      this.stats.forEach(statsItem => this.setStatsText(statsItem.className))
+   },
+
+   setStatsText(item) {
+      this[item].textContent = JSON.parse(localStorage.getItem(item)) || 0
 
    },
 
@@ -41,6 +50,10 @@ const app = {
       this.AIChoice.textContent = this.images[Math.floor(Math.random() * this.images.length)].dataset.type
    },
 
+   setStorage(item) {
+      localStorage.setItem(item, this[item].textContent)
+   },
+
    startGame() {
       if (!this.playerChoice.textContent)
          return alert("What's your choice?")
@@ -48,15 +61,19 @@ const app = {
       if (this.playerChoice.textContent === this.AIChoice.textContent) {
          this.title.textContent = "DRAW!"
          this.draws.textContent++
+         this.setStorage('draws')
       } else if (this.playerChoice.textContent === "paper" && this.AIChoice.textContent === "rock" || this.playerChoice.textContent === "rock" && this.AIChoice.textContent === "scissors" || this.playerChoice.textContent === "scissors" && this.AIChoice.textContent === "paper") {
          this.title.textContent = "WIN! :D"
          this.wins.textContent++
+         this.setStorage('wins')
       } else {
          this.title.textContent = "LOOOOSER! :("
          this.losses.textContent++
+         this.setStorage('losses')
       }
 
       this.games.textContent++
+      this.setStorage('games')
 
       this.button.setAttribute("disabled", "true");
       setTimeout(() => {
